0e7125dd25221040dff80fa3a5c065c698ecf037
Mark-Oasan <markoasan@gmail.com>
2022-12-01 03:32:22 -0800

    NuGet Packages Added - Microsoft.NET.Test.Sdk - NUnit - NUnit3TestAdapter
    Turnoff entry point
    csproj
    <RootNamespace>Assignment5</RootNamespace>
    <GenerateProgramFile>False</GenerateProgramFile>
    
    Added Tests
    SetUp() Initiate Character
    TakeDamage() Reduce HP, set IsAlive false HP < 0 . Health set to zero if damage over remaining heatlh
    RestoreHealth() Add HP, set IsAlive true HP > 0 . Set Health to MaxHealth if the amount over MaxHealth
    
    SetUp() initiate Inventory, Added 1st item
    RemoveItem()
    AddItem()
    Reset()
    Added MaxSlots and AvailableSlots int variables for comparisons

---
+++ Assignment5/Assignment5/CharacterTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Assignment5
+{
+    [TestFixture]
+    internal class CharacterTest
+    {
+        private Character hero;
+        private int HeroHP = 100;
+
+        [SetUp]
+        public void SetUp()
+        {
+            hero = new Character("Bob", RaceCategory.Human, HeroHP);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            hero = null;
+        }
+
+        [Test]
+        public void TakeDamage()
+        {
+            int Damage = 120;
+            hero.TakeDamage(Damage);
+            Assert.AreEqual((Damage > HeroHP) ? 0 : HeroHP - Damage, hero.Health);
+            Assert.IsFalse(hero.IsAlive);
+        }
+
+        [Test]
+        public void RestoreHealth()
+        {
+            int HPtoAdd = 150;
+            hero.RestoreHealth(HPtoAdd);
+            Assert.IsTrue(hero.IsAlive);
+        }
+    }
+}
---
+++ Assignment5/Assignment5/InventoryTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Assignment5
+{
+    [TestFixture]
+    internal class InventoryTest
+    {
+        private Inventory PlayerInventory;
+        private int MaxSlots = 5;
+        private int AvailableSlots;
+        [SetUp]
+        public void SetUp()
+        {
+            PlayerInventory = new Inventory(MaxSlots);
+            PlayerInventory.AddItem(new Item("Wooden Sword", 1, ItemGroup.Equipment));
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            PlayerInventory = null;
+        }
+
+        [Test]
+        public void RemoveItem()
+        {
+            // When set
+            AvailableSlots = PlayerInventory.AvailableSlots;
+            Assert.IsTrue(PlayerInventory.TakeItem("Wooden Sword", out Item found));
+            Assert.IsNotNull(found);
+            Assert.Greater(PlayerInventory.AvailableSlots, AvailableSlots);
+
+            // When null
+            AvailableSlots = PlayerInventory.AvailableSlots;
+            Assert.IsFalse(PlayerInventory.TakeItem("Apple", out Item found1));
+            Assert.IsNull(found1);
+            Assert.AreEqual(PlayerInventory.AvailableSlots, AvailableSlots);
+        }
+
+        [Test]
+        public void AddItem()
+        {
+            AvailableSlots = PlayerInventory.AvailableSlots;
+            Item item = new Item("Blue Berry", 1, ItemGroup.Consumable);
+            Assert.IsTrue(PlayerInventory.AddItem(item));
+            Assert.IsTrue(PlayerInventory.ListAllItems().IndexOf(item) > 0);
+            Assert.Less(PlayerInventory.AvailableSlots, AvailableSlots);
+        }
+
+        [Test]
+        public void Reset()
+        {
+            PlayerInventory.Reset();
+            Assert.AreEqual(PlayerInventory.AvailableSlots, MaxSlots);
+        }
+    }
+}
